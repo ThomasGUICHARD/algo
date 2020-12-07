78882f75ce6ca31374259385edcd80ca4e3228e7
totoKonoha <45076980+totoKonoha@users.noreply.github.com>
2020-12-07 23:41:00 +0100

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,9 +1,9 @@
-
 from pandas import read_csv
 import random
 
-fasta = read_csv('protein.csv', names=[
-    'accession', 'length', 'class', 'fasta']).fasta
+fasta = read_csv('proteine.csv', names=[
+    'accession', 'length', 'class', 'fasta'])
+
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
            'm', 'n', 'e', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -12,17 +12,29 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 def produce_protein():
     ##### For the Database ####
     for i in range(len(fasta) - 1):
-        x = fasta[i]
-        y = fasta[i+1]
-        yield(x, y)
+        x = fasta.fasta[i]
+        y = fasta.fasta[i+1]
+        
+        xlen= fasta.length[i]
+        ylen= fasta.length[i+1]
+        
+            
+        yield(x, y, xlen,ylen)
+
 
 
 def produce_random(count, max_len_x=100, max_len_y=5):
     for _ in range(count):
         x = ''
         y = ''
-        for _ in range(random.randint(2, 100)):
+        for _ in range(random.randint(2, max_len_x)):
             x += letters[random.randint(0, len(letters) - 1)]
-        for _ in range(random.randint(2, 5)):
+        for _ in range(random.randint(2, max_len_y)):
             y += letters[random.randint(0, len(letters) - 1)]
-        yield(x, y)
+        
+
+        x_len=len(x)
+        y_len=len(y)
+        yield(x, y, x_len,y_len)
+
+
